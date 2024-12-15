546a858bad1be74b641961bfc5b078e3f4e1836f
khd325 <hdk325@naver.com>
2024-12-15 16:14:57 +0900

    네트워크 - 프로그램1

---
+++ src/util/MyLogger.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyLogger {
+
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    public static void log(Object obj) {
+        String time = LocalDateTime.now().format(formatter);
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
+
+    }
+}
